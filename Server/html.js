09e614dd085f69b232ef1c93dd02401e31276057
ID3 <dogma.panda@gmail.com>
2021-06-15 17:14:15 +0300

    Fixed bugs with server

--- Server/html.js
+++ Server/html.js
@@ -3,33 +3,62 @@ const fs = require('fs').promises;
 const { parse } = require('querystring');
 
 const host = 'localhost';
-const port = 8000;
+const port = '8000';
 
-let messages = [];
-
-function addNewMessage(user, msg) {
-  messages.push({ "User_name": `${user}`, "User_message": `${msg}` );
-}
+let all_messages_array = [];
+let NumOfMessages = 0;
+let current_message = 0;
+var Num = 0;
 
 // const requestListener = function (req, res) {
-//     if (req.url === "/getMessages")
-
+//     if (req.url === "/getmessages_array")
 // }
 
 const requestListener = function (req, res) {
   console.log(
     `Request: ${req.method}, ${req.url}`
   );
+  /*
   console.log(
     `Request headers: ${JSON.stringify(req.headers)}`
-  );
+  );  
+  */
 
+  let IsMessage = false;
+  let Num = 0;
   let fileName;
   let contentType;
 
   if (req.url === "/") {
     fileName = "index.html";
     contentType = "text/html";
+  }
+  else if (req.url === "/newMessage") {
+    IsMessage = true;
+    res.writeHead(200);
+    let data = "";
+    req.on('data', (chunk) => {
+      data += chunk.toString();
+    })
+    req.on('end', () => {
+      console.log(`Reseived message: ${data}`);
+      if (data !== "") {
+        Message = data;
+        all_messages_array[Num++] = data;
+        window.all_messages_array = all_messages_array;
+      }
+      res.end();
+    })
+    console.log(
+      `Request headers: ${JSON.stringify(req.headers)}`
+    );
+    return;
+  }
+  else if (req.url === "/getMessage") {
+    IsMessage = true;
+    res.writeHead(200);
+    res.end(JSON.stringify(messages_array));
+    console.log("Getting messages_array from user...");
   }
   else if (req.url.endsWith(".css")) {
     fileName = req.url.substr(1);
@@ -40,72 +69,21 @@ const requestListener = function (req, res) {
     return;
   }
 
-  fs.readFile(`${__dirname}/${fileName}`)
-    .then(contents => {
-      res.setHeader("Content-Type", contentType);
-      res.writeHead(200);
-      res.end(contents);
-    })
-    .catch(err => {
-      res.writeHead(500);
-      res.end(err.message);
-      return;
-    });
+  if (!IsMessage)
+    fs.readFile(`${__dirname}/${fileName}`)
+      .then(contents => {
+        res.setHeader("Content-Type", contentType);
+        res.writeHead(200);
+        res.end(contents.toString());
+      })
+      .catch(err => {
+        res.writeHead(500);
+        res.end(err.message);
+        return;
+      });
 };
 
 const server = http.createServer(requestListener);
 server.listen(port);
 
-function collectRequestData(request, callback) {
-  const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-  if (request.headers['content-type'] === FORM_URLENCODED) {
-    let body = '';
-    request.on('data', chunk => {
-      body += chunk.toString();
-    });
-    request.on('end', () => {
-      callback(parse(body));
-    });
-  }
-  else {
-    callback(null);
-  }
-}
-
-async function GetMessage() {
-  for (let i = 0; i < messages.length; i++) {
-    let single_message = messages[i];
-    responce = await fetch(('/getMessage', (req, res) => {
-      const elem = document.createElement("p");
-      const Textnode = document.createTextNode(`This is a new paragraph. ${single_message.User_name}: ${single_message.User_message}`);
-      elem.appendChild(Textnode);
-      const My_body = document.getElementById("chat_proccesing");
-      elem.appendChild(elem);
-    }
-  }
-}
-
-async function SendMessage() {
-  let mes = document.getElementById("mymessage").value;
-  let user_name = document.getElementById("myuser_name").value;
-
-  addNewMessage(mes, user_name);
-
-  const url = 'http://localhost:8000/NewMessage';
-  const data = { User_name: `${user_name}`, User_message: `${mes}` };
-
-  try {
-    const response = await fetch(url, {
-      method: 'POST', // или 'PUT'
-      body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const json = await response.json();
-    console.log('Успех:', JSON.stringify(json));
-  } catch (error) {
-    console.error('Ошибка:', error);
-  }
-}
 
